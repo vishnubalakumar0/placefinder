da642e097622b141282b9f8d1581027abc99946c
vishnubalakumar0 <vishnubalakumar15@gmail.com>
2025-07-30 19:04:09 +0530

    Fix formatting of password reset URL in urlpatterns

--- placefinder/urls.py
+++ placefinder/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
